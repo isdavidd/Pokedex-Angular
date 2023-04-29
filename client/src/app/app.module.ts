d7cd41059e57a7ea254b2cf1dc43cf8a1255227a
Marcus Lima <marcusv2001@gmail.com>
2023-04-29 19:14:18 -0300

    Toolbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,30 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavBarComponent } from './app/components/nav-bar/nav-bar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { PokemoncardComponent } from './components/pokemoncard/pokemoncard.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     NavbarComponent,
-    PokemoncardComponent
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    NgbModule
-=======
+    NgbModule,
     MatToolbarModule,
->>>>>>> 93c9cb13f2ae3a3d55fa2b5b0346b5625d23910d
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/components/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.scss']
+})
+export class PokemonCardComponent {
+
+}
