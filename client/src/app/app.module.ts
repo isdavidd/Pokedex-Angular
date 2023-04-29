e64dd4a07f1defaa2dec9e216f9b6f4c7433bc8d
isdavidd <vini1100@outlook.com>
2023-04-29 18:44:18 -0300

    Created pokemoncard and navbar component folder

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './app/components/nav-bar/nav-bar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PokemoncardComponent } from './components/pokemoncard/pokemoncard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    NavbarComponent,
+    PokemoncardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/components/pokemoncard/pokemoncard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemoncard',
+  templateUrl: './pokemoncard.component.html',
+  styleUrls: ['./pokemoncard.component.scss']
+})
+export class PokemoncardComponent {
+
+}
